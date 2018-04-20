bb234e8e74cab62247f7366aa5b852cdac3b8501
Michael Jones <michael@zaptic.com>
2018-04-20 14:49:57 +0100

    Improve type parsing & move to module
    Separating out the types code as it is quite bulky.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ use nom::multispace;
 
 use nom::types::CompleteStr;
 
+mod types;
+use types::type_;
+
 #[derive(Debug, PartialEq)]
 pub enum ModuleExposing {
     All,
@@ -26,7 +29,7 @@ pub struct Module {
 #[derive(Debug, PartialEq)]
 pub struct FunctionSignature {
     pub name: String,
-    pub type_: Type,
+    pub type_: types::Type,
 }
 
 #[derive(Debug, PartialEq)]
@@ -39,38 +42,6 @@ pub struct FunctionDetails {
     pub signature: Option<FunctionSignature>,
     pub implementation: FunctionImplementation,
 }
-
-// Type
-//
-
-#[derive(Debug, PartialEq)]
-pub enum Type {
-    Function(Vec<Type>),
-    Single(String),
-}
-
-fn combine(first: Type, mut rest: Vec<Type>) -> Type {
-    if rest.is_empty() {
-        first
-    } else {
-        rest.insert(0, first);
-        Type::Function(rest)
-    }
-}
-
-named!(type_<CompleteStr, Type>,
-  do_parse!(
-      first: map!(nom::alpha1, |v| Type::Single(v.0.to_string())) >>
-      rest: many0!(
-          do_parse!(
-              tag!(" -> ") >>
-              name: map!(nom::alphanumeric, |v| Type::Single(v.0.to_string())) >>
-              (name)
-          )
-      ) >>
-      (combine(first, rest))
-  )
-);
 
 named!(module_name<CompleteStr, &str>,
   map!(take_until_s!(" "), |c| c.0)
@@ -142,8 +113,8 @@ named!(contents<CompleteStr, Vec<Declaration>>,
       map!(
           many1!(
               alt!(
-                  map!(char!('\n'), |_| None) |
-                  map!(function, Some)
+                    map!(char!('\n'), |_| None)
+                  | map!(function, Some)
               )
           ),
           filter_none
@@ -178,7 +149,7 @@ fn parse_elm_file() {
                     Declaration::Function(FunctionDetails {
                         signature: Some(FunctionSignature {
                             name: "a".to_string(),
-                            type_: Type::Single("Int".to_string()),
+                            type_: types::Type::Single("Int".to_string()),
                         }),
                         implementation: FunctionImplementation {
                             name: "a".to_string(),
@@ -201,51 +172,6 @@ fn parse_elm_module_declaration() {
                 exposing: ModuleExposing::All,
                 contents: vec![],
             }
-        ))
-    );
-}
-
-// #[cfg(test)]
-// named!(testingalpha<CompleteStr, CompleteStr>,
-//     map!(nom::alphanumeric, |c| c)
-// );
-
-#[test]
-fn parse_elm_type() {
-    //   assert_eq!(
-    //       testingalpha(CompleteStr("Int")),
-    //       Ok((CompleteStr(""), CompleteStr("Int")))
-    //   );
-    assert_eq!(
-        type_(CompleteStr("Int -> Int")),
-        Ok((
-            CompleteStr(""),
-            Type::Function(vec![
-                Type::Single("Int".to_string()),
-                Type::Single("Int".to_string()),
-            ],)
-        ))
-    );
-    assert_eq!(
-        type_(CompleteStr("Int -> Int -> String")),
-        Ok((
-            CompleteStr(""),
-            Type::Function(vec![
-                Type::Single("Int".to_string()),
-                Type::Single("Int".to_string()),
-                Type::Single("String".to_string()),
-            ],)
-        ))
-    );
-    assert_eq!(
-        type_(CompleteStr("String -> Float -> String")),
-        Ok((
-            CompleteStr(""),
-            Type::Function(vec![
-                Type::Single("String".to_string()),
-                Type::Single("Float".to_string()),
-                Type::Single("String".to_string()),
-            ],)
         ))
     );
 }
