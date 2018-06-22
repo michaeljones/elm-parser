fb8b59f9ad8db36419a6949f3cbee51329f83f02
Michael Jones <michael@zaptic.com>
2018-06-22 17:36:49 +0100

    Add start of simple check command
    That begins the process of parsing a file and gathering all the imports
    so that we can begin to check for unused imports. Other checks to
    follow.

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -5,7 +5,8 @@ pub mod expression;
 pub mod helpers;
 pub mod statement;
 
-use ast::statement::{statements, Statement};
+use ast::statement::statements;
+pub use ast::statement::{ExportSet, Statement};
 
 named!(pub file<CompleteStr, Vec<Statement>>,
    terminated!(statements, eof!())
