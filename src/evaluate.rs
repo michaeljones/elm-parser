1412aaaa80059ca6203a1963e35201dc2cc4b710
Michael Jones <m.pricejones@gmail.com>
2020-12-19 18:23:29 +0000

    Evaluate basic binary operations
    Just as a proof of concept. We evaluate the binary operation tree that
    we've built from the expression in the main function.

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1,8 +1,14 @@
-use parser::{Module, Stmt};
+use parser::{Expr, Module, Stmt};
 
 #[derive(Debug)]
 pub enum Error {
     NoMain,
+    UnsupportedOperation,
+}
+
+#[derive(Debug)]
+enum Value {
+    Integer(i32),
 }
 
 pub fn evaluate(module: &Module) -> Result<(), Error> {
@@ -20,7 +26,35 @@ pub fn evaluate(module: &Module) -> Result<(), Error> {
         })
         .ok_or(Error::NoMain)?;
 
-    println!("{:?}", &main_expr);
+    let value = evaluate_expression(&main_expr);
+
+    println!("{:?}", value);
 
     Ok(())
+}
+
+fn evaluate_expression<'a>(expr: &Expr<'a>) -> Result<Value, Error> {
+    match expr {
+        Expr::Integer(int) => Ok(Value::Integer(*int)),
+        Expr::BinOp {
+            operator,
+            left,
+            right,
+        } => evaluate_binary_expression(operator, left, right),
+    }
+}
+
+fn evaluate_binary_expression<'a>(
+    operator: &'a str,
+    left: &Expr<'a>,
+    right: &Expr<'a>,
+) -> Result<Value, Error> {
+    let left_value = evaluate_expression(left)?;
+    let right_value = evaluate_expression(right)?;
+
+    match (operator, left_value, right_value) {
+        ("+", Value::Integer(l), Value::Integer(r)) => Ok(Value::Integer(l + r)),
+        ("*", Value::Integer(l), Value::Integer(r)) => Ok(Value::Integer(l * r)),
+        _ => Err(Error::UnsupportedOperation),
+    }
 }
